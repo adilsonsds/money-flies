99ebc446f0a7926fed40febf0f078eb5eb22380f
Adilson Silva <adilsonsds@icloud.com>
2024-05-26 13:26:33 -0300

    Reset project

--- MoneyFlies.Web/Models/TransactionTag.cs
+++
@@ -1,6 +0,0 @@
-namespace MoneyFlies.Web.Models;
-
-public class TransactionTag
-{
-    public string Name { get; set; }
-}
--- MoneyFlies.Web/Models/TransactionType.cs
+++
@@ -1,7 +0,0 @@
-namespace MoneyFlies.Web.Models;
-
-public enum TransactionType
-{
-    CashInflow = 1,
-    CashOutflow = 2
-}
