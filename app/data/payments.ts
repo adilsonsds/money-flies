57415af54c1c048b5cc82a5bbca68fc36ed6bd6c
Adilson Silva <adilsonsds@icloud.com>
2024-05-28 22:08:18 -0300

    Create Payments service

---
+++ app/data/payments.ts
@@ -0,0 +1,14 @@
+import fs from "fs/promises";
+import { Payment } from "~/types/Payment";
+
+export async function getPayments() : Promise<Payment[]> {
+    const rawFileContent = await fs.readFile("payments.json", "utf-8");
+    const payments: Array<Payment> = JSON.parse(rawFileContent ?? []);
+    return payments;
+}
+
+export async function addPayment(payment: Payment) : Promise<void> {
+    const payments = await getPayments();
+    payments.push(payment);
+    await fs.writeFile("payments.json", JSON.stringify(payments, null, 2));
+}
