b476db14a61defc7a19aedd62e9245ce8e5888b5
Adilson Silva <adilsonsds@icloud.com>
2024-07-21 22:34:26 -0300

    Changing to HashRouter for GithubPages

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
 import ErrorPage from './ErrorPage.tsx';
 import PaymentsList from './routes/PaymentsList.tsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />
   },
   {
-    path: "payments",
+    path: "/payments",
     element: <PaymentsList />,
   },
 ]);
