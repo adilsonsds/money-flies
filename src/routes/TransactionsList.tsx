e68f5079982d4587f6ae145b4c6a9b2c17daae68
Adilson Silva <adilsonsds@icloud.com>
2024-08-23 23:21:23 -0300

    Re-creating transactions table for view

--- src/routes/TransactionsList.tsx
+++ src/routes/TransactionsList.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import PageTitle from "../components/PageTitle";
 import { TransactionItemList } from "../types/Activity";
 import { getTransactions } from "../data/ActivitiesData";
-import { TransactionsTable } from "../components/TransactionsTable";
 
 export default function TransactionsList() {
     const location = useLocation();
@@ -32,10 +31,24 @@ export default function TransactionsList() {
             <PageTitle title="Transactions" />
             {
                 transactions.length > 0 ? (
-                    <TransactionsTable
-                        transactions={transactions}
-                        enableEdit={false}
-                    />
+                    <div className="overflow-x-auto">
+                        <div className="grid grid-cols-12 gap-px bg-gray-400">
+                            <div className="col-span-1 p-2 bg-gray-200 font-bold text-center">#</div>
+                            <div className="col-span-2 p-2 bg-gray-200 font-bold text-center">Date</div>
+                            <div className="col-span-2 p-2 bg-gray-200 font-bold text-center">Category</div>
+                            <div className="col-span-2 p-2 bg-gray-200 font-bold text-center">Amount</div>
+                            <div className="col-span-1 p-2 bg-gray-200 font-bold text-center">Paid</div>
+                            <div className="col-span-3 p-2 bg-gray-200 font-bold text-center">Description</div>
+                            <div className="col-span-1 p-2 bg-gray-200 font-bold text-center">#</div>
+                            {transactions.map((transaction, index) => (
+                                <TableRow
+                                    key={index}
+                                    index={index}
+                                    transaction={transaction}
+                                />
+                            ))}
+                        </div>
+                    </div>
                 ) : (
                     <p className="text-center text-gray-500 dark:text-gray-400">No payments found.</p>
                 )
@@ -51,5 +64,31 @@ export default function TransactionsList() {
                 </Link>
             </div>
         </div>
+    )
+}
+
+type TableRowProps = {
+    index: number;
+    transaction: TransactionItemList;
+}
+
+const TableRow = ({ index, transaction }: TableRowProps) => {
+    return (
+        <>
+            <div className="col-span-1 p-2 bg-white text-center">{index + 1}</div>
+            <div className="col-span-2 p-2 bg-white text-center">{transaction.date}</div>
+            <div className="col-span-2 p-2 bg-white text-center">{transaction.category}</div>
+            <div className="col-span-2 p-2 bg-white text-center align-middle">{transaction.amount}</div>
+            <div className="col-span-1 p-2 bg-white text-center">{transaction.paid ? 'Yes' : 'No'}</div>
+            <div className="col-span-3 p-2 bg-white text-center">
+                {transaction.financialTitle}
+                {transaction.description &&
+                    <div className="text-sm text-gray-500 dark:text-gray-400">{transaction.description}</div>
+                }
+            </div>
+            <div className="col-span-1 p-2 bg-white text-center">
+                <Link to={`/activities/edit/${transaction.financialActivityId}`} className="text-blue-500 dark:text-blue-300 hover:underline">View</Link>
+            </div>
+        </>
     )
 }
