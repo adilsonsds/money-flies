41268ecb0eee940fa931a949c3437cfab5ccef62
Adilson Silva <adilsonsds@icloud.com>
2024-08-28 20:24:23 -0300

    Removing all files

--- src/components/BackButton.tsx
+++
@@ -1,8 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-export function BackButton() {
-    const navigate = useNavigate();
-    return (
-        <button className="text-blue-500" onClick={() => navigate(-1)}>Back</button>
-    );
-} 
