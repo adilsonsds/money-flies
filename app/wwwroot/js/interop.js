a6b3c9a1b9a3c42bdcccbb7ad4987bff6b2bcbcf
Adilson Silva <adilsonsdesousa@gmail.com>
2025-05-25 14:47:13 -0300

    Add downloadFileFromUrl function to facilitate file downloads from a given URL

---
+++ app/wwwroot/js/interop.js
@@ -0,0 +1,8 @@
+window.downloadFileFromUrl = (filename, url) => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
